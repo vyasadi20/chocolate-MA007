e0304a207127ca385775c14234a6f70db9dbfced
vyasadi20 <132550546+vyasadi20@users.noreply.github.com>
2024-02-13 10:34:30 +0100

    Update cart.js

--- cart.js
+++ cart.js
@@ -4,7 +4,28 @@ const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
 // Function that is executed when an "Add to Cart" button is clicked
 const addToCart = (e) => {
   // Find the parent element of the clicked button (a div with class "chocolate-card")
-  const chocolateCard = e.target.parentElement;
+  /*const chocolateCard = e.target.parentElement;*/
+
+  const chocolateCard = e.target.closest(".chocolate-card");
+
+  if (chocolateCard) {
+    // Extract the name of the chocolate from the h3 element within the chocolate card
+    const chocolateName = chocolateCard.querySelector("h3").textContent;
+
+    // Push the chocolate name into the dataLayer
+    dataLayer.push({
+      event: "addToCart",
+      ecommerce: {
+        transaction_name: chocolateName,
+      },
+    });
+
+    // Optionally, you can perform additional actions here, such as updating the UI
+    alert(`Added ${chocolateName} to the cart`);
+    console.log("Added to cart:", chocolateName);
+  }
+};
+
   
   // Extract the name of the chocolate from the h3 element within the chocolate card
   const chocolateName = chocolateCard.querySelector("h3").textContent;
